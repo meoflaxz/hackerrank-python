0ffb2ca4c24748d2467667f94136c350b78d15e9
Meor Hasyim <40785172+meoflaxz@users.noreply.github.com>
2024-01-06 00:41:38 +0800

    comment for solution

--- hackerrank/finding-the-percentage/finding-the-percentage.py
+++ hackerrank/finding-the-percentage/finding-the-percentage.py
@@ -7,5 +7,9 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = input()
     
+
+# student_marks is all the data
+# student_marks[query_name] is scores associated with name
+# find sum of all data, divide by number of scores that exist in that particular name
     average_score = sum(student_marks[query_name]) / len(student_marks[query_name])
     print(format(average_score, ".2f"))
